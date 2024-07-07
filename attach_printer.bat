09988ef1815b450a0b99dff592f36a08acbf7ae0
S-Vighnesh <137196908+S-Vighnesh@users.noreply.github.com>
2024-07-08 00:14:07 +0530

    Add files via upload

---
+++ attach_printer.bat
@@ -0,0 +1,5 @@
+@echo off
+     
+"<Location of the usbip.exe file>\usbip.exe" attach -r <raspberry_pi_ip> -b <busid>
+         
+pause
---
+++ detach_printer.bat
@@ -0,0 +1,5 @@
+@echo off
+     
+"<Location of the usbip.exe file>\usbip.exe" detach -p <port number (usually 0)>
+
+pause
